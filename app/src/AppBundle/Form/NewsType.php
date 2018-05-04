a567b3475556d3b3063d692aef571a89a82deaed
Jędrzej Kubala <j.kubala@sponsor.com.pl>
2018-05-04 10:20:01 +0200

    AddMoreTags button

--- app/src/AppBundle/Form/NewsType.php
+++ app/src/AppBundle/Form/NewsType.php
@@ -14,7 +14,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
